219dc5713806685142ab090629eabd9b97466faf
domaradzkiJs <56085891+domaradzkiJs@users.noreply.github.com>
2019-11-26 18:32:52 +0100

    Updates

---
+++ precache-manifest.6ffc51adc6e4fd760de0de77c1026547.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce44db4320924733fd9f49fd49fd1f29",
+    "url": "/yerba-shop/index.html"
+  },
+  {
+    "revision": "5d367f69c4fc51abe226",
+    "url": "/yerba-shop/static/css/main.31a659ec.chunk.css"
+  },
+  {
+    "revision": "db25e8f8e5daa8c517de",
+    "url": "/yerba-shop/static/js/2.bd61d111.chunk.js"
+  },
+  {
+    "revision": "5d367f69c4fc51abe226",
+    "url": "/yerba-shop/static/js/main.bf428122.chunk.js"
+  },
+  {
+    "revision": "61783524525957daa619",
+    "url": "/yerba-shop/static/js/runtime-main.50c4f00f.js"
+  }
+]);
